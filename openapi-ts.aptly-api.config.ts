a7c9d75c89728065062df458cef6b7aa6a1861bd
Linus Fischer <leicraft@leicraftmc.de>
2025-12-02 11:21:00 +0000

    update

--- openapi-ts.aptly-api.config.ts
+++ openapi-ts.aptly-api.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-    input: 'https://www.aptly.info/swagger/aptly_1.6.2.json',
+    // input: 'https://www.aptly.info/swagger/aptly_1.6.2.json',
+    input: 'src/aptly/api-spec/openapi.json',
     output: 'src/aptly/api-client'
 });
