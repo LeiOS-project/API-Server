f47b25fd7429473916145413f2f5a686439c7bcf
Linus Fischer <leicraft@leicraftmc.de>
2025-12-29 17:20:14 +0000

    update

--- src/tasks/utils.ts
+++ src/tasks/utils.ts
@@ -1,16 +1,21 @@
+import { Logger } from "@hey-api/openapi-ts";
 import { ConfigHandler } from "../utils/config";
 
 export class TaskUtils {
 
     static async getLogsForTask(taskID: number): Promise<string | null> {
-        
-        const logs = Bun.file((ConfigHandler.getConfig()?.LRA_LOG_DIR || "./data/logs") + `/tasks/task-${taskID}.log`);
+        try {
+            const logs = Bun.file((ConfigHandler.getConfig()?.LRA_LOG_DIR || "./data/logs") + `/tasks/task-${taskID}.log`);
 
-        if (!await logs.exists()) {
+            if (!await logs.exists()) {
+                return null;
+            }
+
+            return await logs.text();
+        } catch (err) {
+            Logger.error("Failed to read task logs:", (err as Error).message);
             return null;
         }
-
-        return await logs.text();
     }
 
 }
